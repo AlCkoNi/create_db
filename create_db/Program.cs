2b59d91f2b236888c19a07d662eea0bfa34083f1
AlCkoNi <Tolibjon2004@yandex.ru>
2024-11-07 11:16:52 +0500

    create/full_db

--- create_db/Program.cs
+++ create_db/Program.cs
@@ -1,11 +1,13 @@
-﻿namespace create_db
+﻿using System.Runtime.Versioning;
+using static System.Console;
+
+namespace create_db
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("new commit");
+
         }
     }
 }
